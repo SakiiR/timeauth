9c7ef3eb92834200263715dd6fa8a82902659cc9
SakiiR SakiiR <sakiirlessons@gmail.com>
2017-06-27 16:20:33 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.code import setup
+
+
+setup(
+    name="Time Authentication",
+    version='1.0',
+    description='Python package to exploit time based authentication faster',
+    author='Erwan Dupard',
+    author_email='sakiirlessons@gmail.com',
+    url='https://github.com/SakiiR/timeauth',
+    packages=[]
+)
