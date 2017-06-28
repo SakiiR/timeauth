5e52a48f55e932bfde5d4cd79ff9ba6650b9cd55
SakiiR SakiiR <sakiirlessons@gmail.com>
2017-06-28 12:01:34 +0200

    comments

--- tests/checker.py
+++ tests/checker.py
@@ -5,7 +5,7 @@ from timeauth import TimeAuthChecker
 class ExampleChecker(TimeAuthChecker):
 
     def request(self):
-        context.log_level = 'error'
+        context.log_level = 'error' # Each request code
         s = remote('localhost', 1337)
         s.recvuntil(':')
         s.sendline(self.get_token())
@@ -15,7 +15,7 @@ class ExampleChecker(TimeAuthChecker):
 
     def __init__(self):
         super(self.__class__, self).__init__(
-                   charset="0123456789",
+                   charset="0123456789", # Options
                    token_length=4,
                    hidden_char="*",
                    break_on_time=0.5
