973f2d2762a3a4ee95d18839092400ac85c6d4dd
Erwan Dupard <erwan.dupard@xmco.fr>
2017-06-28 09:56:42 +0200

    Updated gitignore and setup.py

--- setup.py
+++ setup.py
@@ -11,5 +11,15 @@ setup(
     author='Erwan Dupard',
     author_email='sakiirlessons@gmail.com',
     url='https://github.com/SakiiR/timeauth',
+    install_requires=[
+        'pwntools==3.5.1'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Information Security :: Penetration Testing Tool',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
     packages=[]
 )
